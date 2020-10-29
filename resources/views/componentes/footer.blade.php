0cbc90b2a8142a4316cb7a53430dc43c404f76db
Reinaldo Júnior <33222779+ReinaldoJunior96@users.noreply.github.com>
2020-10-28 22:34:09 -0300

    Alterando componentes e criação de tela de login

---
+++ resources/views/componentes/footer.blade.php
@@ -0,0 +1,8 @@
+<footer class="footer">
+    <div class="container-fluid">
+        <div class="copyright float-right">
+            &copy;
+            Reinaldo Junior Dev
+        </div>
+    </div>
+</footer>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\EstoqueController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
